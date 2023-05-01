0e31e8575b46baa7390ce95d15a0fb3ea2c05db5
Michael Ko. Gajda <draget@speciesm.net>
2023-05-01 12:43:57 +0200

    Cleanup comments
    Comments should have one whitespace leading.

--- winjuvanate.ps1
+++ winjuvanate.ps1
@@ -1,4 +1,4 @@
-#Defining preferences variables
+# Defining preferences variables
 Write-Output "Loading configuration from config.json..."
 $config = (Get-Content "config.json" -Raw) | ConvertFrom-Json
 $wantedImageName = $config.WantedWindowsEdition
@@ -7,7 +7,7 @@ $unwantedWindowsPackages = $config.WindowsPackagesToRemove
 $pathsToDelete = $config.PathsToDelete
 $windowsIsoDownloaderReleaseUrl = $config.WindowsIsoDownloaderReleaseUrl
 
-#Defining system variables
+# Defining system variables
 Write-Output "Creating needed variables..."
 $rootWorkdir = "c:\tiny11\"
 $isoFolder = $rootWorkdir + "iso\"
@@ -16,8 +16,8 @@ $bootImageFolder = $rootWorkdir + "bootimage\"
 $toolsFolder = $rootWorkdir + "tools\"
 $isoPath = "c:\windows11.iso"
 $yes = (cmd /c "choice <nul 2>nul")[1]
-#The $yes variable gets the "y" from "yes" (or corresponding letter in the language your computer is using).
-#It is used to answer automatically to the "takeown" command, because the answer choices are localized which is not handy at all.
+# The $yes variable gets the "y" from "yes" (or corresponding letter in the language your computer is using).
+# It is used to answer automatically to the "takeown" command, because the answer choices are localized which is not handy at all.
 
 md $rootWorkdir | Out-Null
 md ($toolsFolder + "WindowsIsoDownloader\") | Out-Null
@@ -28,44 +28,44 @@ Write-Output "Extracting WindowsIsoDownloader release..."
 Expand-Archive -Path WindowsIsoDownloader.zip -DestinationPath ($toolsFolder + "WindowsIsoDownloader\")
 Remove-Item WindowsIsoDownloader.zip | Out-Null
 
-#Downloading the Windows 11 ISO using WindowsIsoDownloader
+# Downloading the Windows 11 ISO using WindowsIsoDownloader
 Write-Output "Downloading Windows 11 iso file from Microsoft using WindowsIsoDownloader..."
 $isoDownloadProcess = (Start-Process ($toolsFolder + "WindowsIsoDownloader\WindowsIsoDownloader.exe") -NoNewWindow -Wait -WorkingDirectory ($toolsFolder + "WindowsIsoDownloader\") -PassThru)
 
 if ($isoDownloadProcess.ExitCode -eq 0) {
-	#Mount the Windows 11 ISO
+	# Mount the Windows 11 ISO
 	Write-Output "Mounting the original iso..."
 	$mountResult = Mount-DiskImage -ImagePath $isoPath
 	$isoDriveLetter = ($mountResult | Get-Volume).DriveLetter
 
-	#Creating needed temporary folders
+	# Creating needed temporary folders
 	Write-Output "Creating temporary folders..."
 	md $isoFolder | Out-Null
 	md $installImageFolder | Out-Null
 	md $bootImageFolder | Out-Null
 
-	#Copying the ISO files to the ISO folder
+	# Copying the ISO files to the ISO folder
 	Write-Output "Copying the content of the original iso to the work folder..."
 	cp -Recurse ($isoDriveLetter + ":\*") $isoFolder | Out-Null
 
-	#Unmounting the original ISO since we don't need it anymore (we have a copy of the content)
+	# Unmounting the original ISO since we don't need it anymore (we have a copy of the content)
 	Write-Output "Unmounting the original iso..."
 	Dismount-DiskImage -ImagePath $isoPath | Out-Null
 
 	################# Beginning of install.wim patches ##################
-	#Getting the wanted image index
+	# Getting the wanted image index
 	$wantedImageIndex = Get-WindowsImage -ImagePath ($isoFolder + "sources\install.wim") | where-object { $_.ImageName -eq $wantedImageName } | Select-Object -ExpandProperty ImageIndex
 
-	#Mounting the WIM image
+	# Mounting the WIM image
 	Write-Output "Mounting the install.wim image..."
 	Set-ItemProperty -Path ($isoFolder + "sources\install.wim") -Name IsReadOnly -Value $false | Out-Null
 	Mount-WindowsImage -ImagePath ($isoFolder + "sources\install.wim") -Path $installImageFolder -Index $wantedImageIndex | Out-Null
 
-	#Detecting provisionned app packages
+	# Detecting provisioned app packages
 	Write-Output "Removing unwanted app packages from the install.wim image..."
 	$detectedProvisionnedPackages = Get-AppxProvisionedPackage -Path $installImageFolder
 
-	#Removing unwanted provisionned app packages
+	# Removing unwanted provisioned app packages
 	foreach ($detectedProvisionnedPackage in $detectedProvisionnedPackages) {
 		foreach ($unwantedProvisionnedPackage in $unwantedProvisionnedPackages) {
 			if ($detectedProvisionnedPackage.PackageName.Contains($unwantedProvisionnedPackage)) {
@@ -74,11 +74,11 @@ if ($isoDownloadProcess.ExitCode -eq 0) {
 		}
 	}
 
-	#Detecting windows packages
+	# Detecting windows packages
 	Write-Output "Removing unwanted windows packages from the install.wim image..."
 	$detectedWindowsPackages = Get-WindowsPackage -Path $installImageFolder
 
-	#Removing unwanted windows packages
+	# Removing unwanted windows packages
 	foreach ($detectedWindowsPackage in $detectedWindowsPackages) {
 		foreach ($unwantedWindowsPackage in $unwantedWindowsPackages) {
 			if ($detectedWindowsPackage.PackageName.Contains($unwantedWindowsPackage)) {
@@ -124,19 +124,19 @@ if ($isoDownloadProcess.ExitCode -eq 0) {
 	reg unload HKLM\installwim_SOFTWARE | Out-Null
 	reg unload HKLM\installwim_SYSTEM | Out-Null
 
-	#Copying the setup config file
+	# Copying the setup config file
 	Write-Output "Placing the autounattend.xml file in the install.wim image..."
 	[System.IO.File]::Copy((Get-ChildItem .\tools\autounattend.xml).FullName, ($installImageFolder + "Windows\System32\Sysprep\autounattend.xml"), $true) | Out-Null
 
-	#Unmount the install.wim image
+	# Unmount the install.wim image
 	Write-Output "Unmounting the install.wim image..."
 	Dismount-WindowsImage -Path $installImageFolder -Save | Out-Null
 
-	#Moving the wanted image index to a new image
+	# Moving the wanted image index to a new image
 	Write-Output "Creating a clean install.wim image without all unnecessary indexes..."
 	Export-WindowsImage -SourceImagePath ($isoFolder + "sources\install.wim") -SourceIndex $wantedImageIndex -DestinationImagePath ($isoFolder + "sources\install_patched.wim") -CompressionType max | Out-Null
 
-	#Delete the old install.wim and rename the new one
+	# Delete the old install.wim and rename the new one
 	rm ($isoFolder + "sources\install.wim") | Out-Null
 	Rename-Item -Path ($isoFolder + "sources\install_patched.wim") -NewName "install.wim" | Out-Null
 	################# Ending of install.wim patches ##################
@@ -159,29 +159,29 @@ if ($isoDownloadProcess.ExitCode -eq 0) {
 	reg unload HKLM\bootwim_NTUSER | Out-Null
 	reg unload HKLM\bootwim_SYSTEM | Out-Null
 
-	#Unmount the boot.wim image
+	# Unmount the boot.wim image
 	Write-Output "Unmounting the boot.wim image..."
 	Dismount-WindowsImage -Path $bootImageFolder -Save | Out-Null
 
-	#Moving the wanted image index to a new image
+	# Moving the wanted image index to a new image
 	Write-Output "Creating a clean boot.wim image without all unnecessary indexes..."
 	Export-WindowsImage -SourceImagePath ($isoFolder + "sources\boot.wim") -SourceIndex 2 -DestinationImagePath ($isoFolder + "sources\boot_patched.wim") -CompressionType max | Out-Null
 
-	#Delete the old boot.wim and rename the new one
+	# Delete the old boot.wim and rename the new one
 	rm ($isoFolder + "sources\boot.wim") | Out-Null
 	Rename-Item -Path ($isoFolder + "sources\boot_patched.wim") -NewName "boot.wim" | Out-Null
 	################# Ending of boot.wim patches ##################
 
-	#Copying the setup config file to the iso copy folder
+	# Copying the setup config file to the iso copy folder
 	[System.IO.File]::Copy((Get-ChildItem .\tools\autounattend.xml).FullName, ($isoFolder + "autounattend.xml"), $true) | Out-Null
 
-	#Building the new trimmed and patched iso file
+	# Building the new trimmed and patched iso file
 	Write-Output "Building the tiny11.iso file..."
 	.\tools\oscdimg.exe -m -o -u2 -udfver102 -bootdata:("2#p0,e,b" + $isoFolder + "boot\etfsboot.com#pEF,e,b" + $isoFolder + "efi\microsoft\boot\efisys.bin") $isoFolder c:\tiny11.iso | Out-Null
 } else {
 	Write-Output "Unable to build the tiny11 iso (an error occured while trying to download the original iso using WindowsIsoDownloader)."
 }
 
-#Cleaning the folders used during the process
+# Cleaning the folders used during the process
 Write-Output "Removing work folders..."
 Remove-Item $rootWorkdir -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
